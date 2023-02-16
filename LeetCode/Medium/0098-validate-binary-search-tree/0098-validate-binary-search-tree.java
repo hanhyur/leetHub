ff5c8331a8b4f832ca9f2ac2af8505ea3e070e8a
Sunwoo Han (Hyur) <thankowl777@kakao.com>
2023-02-16 15:48:14 +0900

    Time: 0 ms (100.00%), Space: 41.6 MB (93.81%) - LeetHub

--- LeetCode/Medium/0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ LeetCode/Medium/0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,30 +14,24 @@
  * }
  */
 class Solution {
+    // BST가 성립하기 위해서는 root의 left(하위 node 포함)는 root보다 작아야하고
+    // root의 right(하위 node 포함)는 root보다 커야한다.
     public boolean isValidBST(TreeNode root) {
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public boolean isValid(TreeNode root, long min, long max) {
         if (root == null) {
             return true;
         }
         
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode pre = null;
-        
-        while (root != null || !stack.isEmpty()) {
-            while (root != null) {
-                stack.push(root);
-                root = root.left;
-            }
-            
-            root = stack.pop();
-            
-            if(pre != null && root.val <= pre.val) {
-                return false;
-            }
-            
-            pre = root;
-            root = root.right;
+        // root의 값이 범위를 벗어났다면
+        if (root.val <= min || root.val >= max) {
+            return false;
         }
         
-        return true;
+        // left와 right 모두 만족하는가?
+        return isValid(root.left, min, root.val)
+            && isValid(root.right, root.val, max);
     }
 }
