219cbf194a71c9e969697bd38e903f473e74fd91
Sunwoo Han (Hyur) <thankowl777@kakao.com>
2023-02-20 23:25:00 +0900

    Time: 0 ms (100.00%), Space: 42.6 MB (48.54%) - LeetHub

--- LeetCode/Medium/0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ LeetCode/Medium/0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,22 +1,29 @@
 class Solution {
     // 결과 값이 가장 큰 부분수열을 구한다.
-    // 단, 연속되어야만 한다.
     public int maxProduct(int[] nums) {
-        int max = nums[0], min = nums[0], ans = nums[0];
-        int n = nums.length;
-        
-        for (int i = 1; i < n; i++) {
-            // max, min 서로 바꾸기
-            if (nums[i] < 0){
-                int temp = max;
-                max = min;
-                min = temp;
-            }
-            
-            max = Math.max(nums[i], max * nums[i]);
-            min = Math.min(nums[i], min * nums[i]);
+        int ans =  Integer.MIN_VALUE;
+        int prod = 1;
 
-            ans = Math.max(ans, max);
+        for (int i = 0; i < nums.length; i++){
+            prod *= nums[i];
+            
+            ans = Math.max(ans, prod);
+            
+            if (prod == 0) {
+                prod = 1;
+            }
+        }
+        
+        prod = 1;
+        
+        for (int i = nums.length - 1; i >= 0; i--){
+            prod *= nums[i];
+            
+            ans = Math.max(ans, prod);
+            
+            if (prod == 0) {
+                prod = 1;
+            }
         }
         
         return ans;
