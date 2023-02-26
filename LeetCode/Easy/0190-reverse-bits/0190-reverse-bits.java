d5a9cc332bb85f7495b5b9063caa785ae74e2129
Sunwoo Han (Hyur) <thankowl777@kakao.com>
2023-02-27 00:02:55 +0900

    Time: 0 ms (100.00%), Space: 41.9 MB (42.86%) - LeetHub

--- LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.java
+++ LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.java
@@ -1,12 +1,13 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int ans = 0;
+        int ans = n;
         
-        for(int i = 0; i < 32; i++){
-    	    ans = (ans << 1) | (n & 1);         
-    	    n = n >> 1;                  
-        }
+        ans = ((ans & 0xffff0000) >>> 16) | ((ans & 0x0000ffff) << 16);
+        ans = ((ans & 0xff00ff00) >>> 8) | ((ans & 0x00ff00ff) << 8);
+        ans = ((ans & 0xf0f0f0f0) >>> 4) | ((ans & 0x0f0f0f0f) << 4);
+        ans = ((ans & 0xcccccccc) >>> 2) | ((ans & 0x33333333) << 2);
+        ans = ((ans & 0xaaaaaaaa) >>> 1) | ((ans & 0x55555555) << 1);
         
         return ans;
     }
