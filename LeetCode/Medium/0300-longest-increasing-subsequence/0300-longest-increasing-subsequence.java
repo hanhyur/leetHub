d343e36823e3731a6d884b36176826caa2ccdc59
Sunwoo Han (Hyur) <thankowl777@kakao.com>
2023-03-02 22:45:15 +0900

    Time: 1 ms (100.00%), Space: 42.4 MB (31.40%) - LeetHub

--- LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,24 +1,23 @@
 class Solution {
     // 증가하는 형태를 가진 가장 긴 부분수열의 길이를 구한다
     public int lengthOfLIS(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
+        int n = nums.length, ans = 0;
+        int[] dp = new int[n];
         
-        int[] dp = new int[nums.length];
-        int ans = 1;
-        
-        for (int i = 0; i < nums.length; i++) {
-            dp[i] = 1; // 케이스 중 가장 짧은 경우 1이기 때문
-            
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    ans = Math.max(dp[i], ans);
+        for (int i = 0; i < n; i++) {
+            if (ans == 0 || dp[ans - 1] < nums[i]) {
+                dp[ans++] = nums[i]; 
+            } else {
+                var j = ans - 1;
+                
+                while(j >= 0 && dp[j] >= nums[i]) {
+                    j--; 
                 }
-            }
+                
+                dp[++j] = nums[i]; 
+            } 
         }
         
-        return ans; 
+        return ans;
     }
 }
