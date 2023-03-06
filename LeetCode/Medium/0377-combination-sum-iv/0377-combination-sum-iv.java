0ecb3566e1fefad064b353280ebc6e5ab5367889
Sunwoo Han (Hyur) <thankowl777@kakao.com>
2023-03-06 21:11:58 +0900

    Time: 1 ms (79.85%), Space: 39.7 MB (64.20%) - LeetHub

---
+++ LeetCode/Medium/0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,21 @@
+class Solution {
+    // bottom-up
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        
+        for (int i = 0; i < target; i++) {
+            if (dp[i] == 0) {
+                continue;
+            }
+            
+            for (int num : nums) {
+                if (num + i <= target) {
+                    dp[i + num] += dp[i];
+                }
+            }
+        }
+        
+        return dp[target];
+    }
+}
